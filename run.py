47f39edf7d36b5c4257da3f37d0a700da8523159
Aurelio Barrios <aubarrio@ucsd.edu>
2020-10-29 15:18:31 -0700

    implemented

---
+++ run.py
@@ -0,0 +1,26 @@
+import argparse
+import sys
+import json
+
+from src.data.etl import complete
+
+def main():
+
+    with open('config/params.json') as fh:
+        data_cfg = json.load(fh)
+
+    if targets:
+        assert len(targets) >= 2, "Make sure to provide a feature and edge filepath! Features filepath first followed by edge filepath."
+
+        features_fp = targets[0]
+        edges_fp = targets[1]
+
+        data_cfg['fp_features'] = features_fp
+        data_cfg['fp_edges'] = edges_fp
+
+    complete(**data_cfg)
+
+
+if __name__ == '__main__':
+    targets = sys.argv[1:]
+    main()
---
+++ src/data/etl.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import json
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+
+def read_edges(source, path):
+    """
+    Reads in edge data
+    """
+    if source == 'cora':
+        d = pd.read_csv(path, sep='\t', header=None)
+    elif source == 'snap':
+        d = pd.read_csv(path)
+    return d
+
+def read_features(source, path):
+    """
+    Returns a dataframe of the data inputed
+    """
+    if source == 'cora':
+        d = pd.read_csv(path, sep='\t', header=None)
+        d = d.rename(columns={0: 'index'})
+    elif source == 'snap':
+        with open(path) as json_data:
+            data = json.load(json_data)
+        d = pd.DataFrame.from_dict(data, orient='index')
+        d['index'] = d.index
+        d = d.reset_index(drop=True)
+    else:
+        d = np.NaN
+    return d
+
+def index_nodes(d):
+    index = d['index']
+    in_d = {}
+    for i in np.arange(len(index)):
+        in_d[int(index[i])] = i
+        i += 1
+    return in_d
+
+def create_adj(in_d, d_edge):
+    adj = np.zeros((len(in_d), len(in_d)), dtype='float32')
+    rows = d_edge.iloc[:, 0]
+    cols = d_edge.iloc[:, 1]
+    for i in np.arange(len(d_edge)):
+        adj[in_d[rows[i]], in_d[cols[i]]] = 1.0
+    return adj
+
+def complete(source, fp_features, fp_edges, outpath):
+    # print("Source is: " + source)
+    # print("fp_features is: " + fp_features)
+    # print("fp_edges is: " + fp_edges)
+    # print("outpath is: " + outpath)
+    edge_df = read_edges(source, fp_edges)
+    feat_df = read_features(source, fp_features)
+
+    in_d = index_nodes(feat_df)
+    adj = create_adj(in_d, edge_df)
+
+    G = nx.from_numpy_matrix(adj, nx.DiGraph())
+    plt.figure(figsize=(10,10))
+    nx.draw(G, node_size=10, edge_size=1)
+    plt.show()
+    return -1
